aa010efd10303f9324fd6ac23c02877d76c8c2e7
marconius <marco@cuthemustard.com>
2014-07-18 21:51:41 -0400

    Removing admin registration to make it "abstract"

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from blog.models import *
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -12,7 +11,6 @@ class PostAdmin(admin.ModelAdmin):
     list_filter   = ('publish', 'categories', 'status')
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
-admin.site.register(Post, PostAdmin)
 
 
 class BlogRollAdmin(admin.ModelAdmin):
